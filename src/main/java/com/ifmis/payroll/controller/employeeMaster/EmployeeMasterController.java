20be6c9cb852eb538a7d7c4da99353bd307412eb
sagarmahajan-deloitte <sagarmahajan@deloitte.com>
2026-04-09 12:36:42 +0530

    create emp add

--- src/main/java/com/ifmis/payroll/controller/employeeMaster/EmployeeMasterController.java
+++ src/main/java/com/ifmis/payroll/controller/employeeMaster/EmployeeMasterController.java
@@ -1,6 +1,8 @@
 package com.ifmis.payroll.controller.employeeMaster;
 
 import com.ifmis.payroll.dto.EmployeeRequestDto;
+import com.ifmis.payroll.entity.Employee;
+import com.ifmis.payroll.service.EmployeeService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -14,9 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class EmployeeMasterController {
 
+    private final EmployeeService employeeService;
 
     @PostMapping
-    public ResponseEntity<?> createEmployee(@Valid @RequestBody EmployeeRequestDto request) {
-        return null;
+    public ResponseEntity<Employee> createEmployee(@Valid @RequestBody EmployeeRequestDto request) {
+        Employee employee = employeeService.createEmployee(request);
+        return ResponseEntity.ok(employee);
     }
 }
